1ccdeee1a58a0da060b948e50f5e2f7b95289ad1
yaoxinmeng <yaoxinmeng@gmail.com>
2025-06-01 18:37:12 +0800

    added home page draft

---
+++ frontend/src/routes/+page.server.ts
@@ -0,0 +1,16 @@
+import type { PageServerLoad, Actions } from './$types';
+import db from '$db/mongo';
+
+export const load: PageServerLoad = async () => {
+    let cursor = db.collection('favourites').find();
+    let favourites = await cursor.toArray();
+	return {
+		favourites: favourites
+	};
+};
+
+export const actions = {
+	default: async ({ request }) => {
+		
+	}
+} satisfies Actions;
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+    import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+    console.log('Page data:', data);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-screen w-full px-16 py-8">
+    <h1 class="text-lg font-semibold">Watchlist</h1>
+    <div class="flex justify-end">
+        <button class="bg-blue-300 rounded-full p-4 cursor-pointer" on:click>Add New +</button>
+    </div>
+</div>
