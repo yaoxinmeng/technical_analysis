a879a7ccca99bd6f6dfb3e0b653da5993ba6bf8d
yaoxinmeng <yaoxinmeng@gmail.com>
2025-10-11 23:02:58 +0800

    Added headless mode

--- backend/scraper/app/service/scraper.py
+++ backend/scraper/app/service/scraper.py
@@ -2,15 +2,17 @@ from playwright.sync_api import sync_playwright, TimeoutError
 from loguru import logger
 from app.core.config import settings
 
+EXECUTABLE_PATH = "/usr/bin/chromium"
+
 def playwright_scrape(url: str, headless: bool = False, timeout: int = settings.PLAYWRIGHT_TIMEOUT) -> str:
     """
     Scrape the contents of a page using the Playwright Sync API.
     """
     with sync_playwright() as playwright:
         if headless:
-            browser = playwright.chromium.launch(channel="chromium")
+            browser = playwright.chromium.launch(executable_path=EXECUTABLE_PATH, channel="chromium")
         else:
-            browser = playwright.chromium.launch(headless=False)
+            browser = playwright.chromium.launch(executable_path=EXECUTABLE_PATH, headless=False)
         context = browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
         page = context.new_page()
         try:
